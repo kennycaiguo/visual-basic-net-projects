a1724aea0f67b39101f3caf06dbdd67e82faf923
kyematzen <kyleofficalhd@gmail.com>
2020-11-12 10:44:47 -0500

    Added new project: Hurricane Wind Speed Live

---
+++ Book/Hurricane Wind Speed/Hurricane Wind Speed/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmHurricane
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblSpeed = New System.Windows.Forms.Label()
+        Me.txtBoxSpeed = New System.Windows.Forms.TextBox()
+        Me.txtBoxRating = New System.Windows.Forms.TextBox()
+        Me.timerRatingUpdater = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblSpeed
+        '
+        Me.lblSpeed.AutoSize = True
+        Me.lblSpeed.Location = New System.Drawing.Point(13, 13)
+        Me.lblSpeed.Name = "lblSpeed"
+        Me.lblSpeed.Size = New System.Drawing.Size(67, 13)
+        Me.lblSpeed.TabIndex = 0
+        Me.lblSpeed.Text = "Wind speed:"
+        '
+        'txtBoxSpeed
+        '
+        Me.txtBoxSpeed.Location = New System.Drawing.Point(86, 10)
+        Me.txtBoxSpeed.Name = "txtBoxSpeed"
+        Me.txtBoxSpeed.Size = New System.Drawing.Size(100, 20)
+        Me.txtBoxSpeed.TabIndex = 1
+        '
+        'txtBoxRating
+        '
+        Me.txtBoxRating.Location = New System.Drawing.Point(16, 45)
+        Me.txtBoxRating.Name = "txtBoxRating"
+        Me.txtBoxRating.ReadOnly = True
+        Me.txtBoxRating.Size = New System.Drawing.Size(170, 20)
+        Me.txtBoxRating.TabIndex = 2
+        '
+        'timerRatingUpdater
+        '
+        Me.timerRatingUpdater.Enabled = True
+        '
+        'frmHurricane
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(202, 77)
+        Me.Controls.Add(Me.txtBoxRating)
+        Me.Controls.Add(Me.txtBoxSpeed)
+        Me.Controls.Add(Me.lblSpeed)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmHurricane"
+        Me.Text = "Hurricane (LIVE)"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblSpeed As Label
+    Friend WithEvents txtBoxSpeed As TextBox
+    Friend WithEvents txtBoxRating As TextBox
+    Friend WithEvents timerRatingUpdater As Timer
+End Class
---
+++ Book/Hurricane Wind Speed/Hurricane Wind Speed/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class frmHurricane
+    Private Sub timerRatingUpdater_Tick(sender As Object, e As EventArgs) Handles timerRatingUpdater.Tick
+        Dim rating = txtBoxSpeed.Text
+
+        If IsNumeric(rating) Then
+            Dim number As Integer = CInt(rating)
+
+            Select Case number
+                Case 74 To 95
+                    txtBoxRating.Text = "Category One hurricane."
+                Case 96 To 110
+                    txtBoxRating.Text = "Category Two hurricane."
+                Case 111 To 130
+                    txtBoxRating.Text = "Category Three hurricane."
+                Case 131 To 155
+                    txtBoxRating.Text = "Category Four hurricane."
+                Case Is > 155
+                    txtBoxRating.Text = "Category Five hurricane."
+                Case Else
+                    txtBoxRating.Text = "No hurricane."
+            End Select
+
+            Return
+        End If
+
+        txtBoxRating.Text = "No hurricane."
+    End Sub
+End Class
