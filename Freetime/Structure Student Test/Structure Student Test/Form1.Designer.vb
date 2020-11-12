3880fa8537793d08627334557d34976152b126b5
kyematzen <kyleofficalhd@gmail.com>
2020-11-11 20:50:11 -0500

    Added in progress project that utilizes structures and will have a search function to find details about specific students.

---
+++ Freetime/Structure Student Test/Structure Student Test/Form1.Designer.vb
@@ -0,0 +1,437 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmStructureTest
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblStudentInfo = New System.Windows.Forms.Label()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblAge = New System.Windows.Forms.Label()
+        Me.lblGradYear = New System.Windows.Forms.Label()
+        Me.lblDebugInformation = New System.Windows.Forms.Label()
+        Me.listBoxDebugInformation = New System.Windows.Forms.ListBox()
+        Me.lblResults = New System.Windows.Forms.Label()
+        Me.listBoxResults = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.lblGPA = New System.Windows.Forms.Label()
+        Me.groupBoxGradYear = New System.Windows.Forms.GroupBox()
+        Me.rButtonGradYear24 = New System.Windows.Forms.RadioButton()
+        Me.rButtonGradYear23 = New System.Windows.Forms.RadioButton()
+        Me.rButtonGradYear22 = New System.Windows.Forms.RadioButton()
+        Me.rButtonGradYear21 = New System.Windows.Forms.RadioButton()
+        Me.groupBoxAge = New System.Windows.Forms.GroupBox()
+        Me.rButtonAge18 = New System.Windows.Forms.RadioButton()
+        Me.rButtonAge17 = New System.Windows.Forms.RadioButton()
+        Me.rButtonAge16 = New System.Windows.Forms.RadioButton()
+        Me.rButtonAge15 = New System.Windows.Forms.RadioButton()
+        Me.txtBoxFirstName = New System.Windows.Forms.TextBox()
+        Me.txtBoxLastName = New System.Windows.Forms.TextBox()
+        Me.groupBoxGPA = New System.Windows.Forms.GroupBox()
+        Me.rButtonGPA4 = New System.Windows.Forms.RadioButton()
+        Me.rButtonGPA2 = New System.Windows.Forms.RadioButton()
+        Me.rButtonGPA3 = New System.Windows.Forms.RadioButton()
+        Me.rButtonGPA1 = New System.Windows.Forms.RadioButton()
+        Me.lblSearch = New System.Windows.Forms.Label()
+        Me.txtBoxSearch = New System.Windows.Forms.TextBox()
+        Me.timerDebugClearing = New System.Windows.Forms.Timer(Me.components)
+        Me.timerSearch = New System.Windows.Forms.Timer(Me.components)
+        Me.groupBoxGradYear.SuspendLayout()
+        Me.groupBoxAge.SuspendLayout()
+        Me.groupBoxGPA.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblStudentInfo
+        '
+        Me.lblStudentInfo.AutoSize = True
+        Me.lblStudentInfo.Location = New System.Drawing.Point(13, 13)
+        Me.lblStudentInfo.Name = "lblStudentInfo"
+        Me.lblStudentInfo.Size = New System.Drawing.Size(99, 13)
+        Me.lblStudentInfo.TabIndex = 0
+        Me.lblStudentInfo.Text = "Student Information"
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(13, 39)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(60, 13)
+        Me.lblFirstName.TabIndex = 1
+        Me.lblFirstName.Text = "First Name:"
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(146, 40)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(61, 13)
+        Me.lblLastName.TabIndex = 2
+        Me.lblLastName.Text = "Last Name:"
+        '
+        'lblAge
+        '
+        Me.lblAge.AutoSize = True
+        Me.lblAge.Location = New System.Drawing.Point(13, 80)
+        Me.lblAge.Name = "lblAge"
+        Me.lblAge.Size = New System.Drawing.Size(29, 13)
+        Me.lblAge.TabIndex = 3
+        Me.lblAge.Text = "Age:"
+        '
+        'lblGradYear
+        '
+        Me.lblGradYear.AutoSize = True
+        Me.lblGradYear.Location = New System.Drawing.Point(146, 85)
+        Me.lblGradYear.Name = "lblGradYear"
+        Me.lblGradYear.Size = New System.Drawing.Size(87, 13)
+        Me.lblGradYear.TabIndex = 4
+        Me.lblGradYear.Text = "Graduation Year:"
+        '
+        'lblDebugInformation
+        '
+        Me.lblDebugInformation.AutoSize = True
+        Me.lblDebugInformation.Location = New System.Drawing.Point(404, 13)
+        Me.lblDebugInformation.Name = "lblDebugInformation"
+        Me.lblDebugInformation.Size = New System.Drawing.Size(94, 13)
+        Me.lblDebugInformation.TabIndex = 5
+        Me.lblDebugInformation.Text = "Debug Information"
+        '
+        'listBoxDebugInformation
+        '
+        Me.listBoxDebugInformation.FormattingEnabled = True
+        Me.listBoxDebugInformation.Location = New System.Drawing.Point(407, 34)
+        Me.listBoxDebugInformation.Name = "listBoxDebugInformation"
+        Me.listBoxDebugInformation.Size = New System.Drawing.Size(372, 537)
+        Me.listBoxDebugInformation.TabIndex = 6
+        '
+        'lblResults
+        '
+        Me.lblResults.AutoSize = True
+        Me.lblResults.Location = New System.Drawing.Point(15, 252)
+        Me.lblResults.Name = "lblResults"
+        Me.lblResults.Size = New System.Drawing.Size(42, 13)
+        Me.lblResults.TabIndex = 7
+        Me.lblResults.Text = "Results"
+        '
+        'listBoxResults
+        '
+        Me.listBoxResults.FormattingEnabled = True
+        Me.listBoxResults.Location = New System.Drawing.Point(18, 268)
+        Me.listBoxResults.Name = "listBoxResults"
+        Me.listBoxResults.Size = New System.Drawing.Size(376, 303)
+        Me.listBoxResults.TabIndex = 8
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(16, 157)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 9
+        Me.btnAdd.Text = "Add Student"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'lblGPA
+        '
+        Me.lblGPA.AutoSize = True
+        Me.lblGPA.Location = New System.Drawing.Point(327, 40)
+        Me.lblGPA.Name = "lblGPA"
+        Me.lblGPA.Size = New System.Drawing.Size(32, 13)
+        Me.lblGPA.TabIndex = 10
+        Me.lblGPA.Text = "GPA:"
+        '
+        'groupBoxGradYear
+        '
+        Me.groupBoxGradYear.Controls.Add(Me.rButtonGradYear24)
+        Me.groupBoxGradYear.Controls.Add(Me.rButtonGradYear23)
+        Me.groupBoxGradYear.Controls.Add(Me.rButtonGradYear22)
+        Me.groupBoxGradYear.Controls.Add(Me.rButtonGradYear21)
+        Me.groupBoxGradYear.Location = New System.Drawing.Point(149, 96)
+        Me.groupBoxGradYear.Name = "groupBoxGradYear"
+        Me.groupBoxGradYear.Size = New System.Drawing.Size(140, 55)
+        Me.groupBoxGradYear.TabIndex = 13
+        Me.groupBoxGradYear.TabStop = False
+        '
+        'rButtonGradYear24
+        '
+        Me.rButtonGradYear24.AutoSize = True
+        Me.rButtonGradYear24.Location = New System.Drawing.Point(85, 29)
+        Me.rButtonGradYear24.Name = "rButtonGradYear24"
+        Me.rButtonGradYear24.Size = New System.Drawing.Size(49, 17)
+        Me.rButtonGradYear24.TabIndex = 3
+        Me.rButtonGradYear24.TabStop = True
+        Me.rButtonGradYear24.Text = "2024"
+        Me.rButtonGradYear24.UseVisualStyleBackColor = True
+        '
+        'rButtonGradYear23
+        '
+        Me.rButtonGradYear23.AutoSize = True
+        Me.rButtonGradYear23.Location = New System.Drawing.Point(9, 29)
+        Me.rButtonGradYear23.Name = "rButtonGradYear23"
+        Me.rButtonGradYear23.Size = New System.Drawing.Size(49, 17)
+        Me.rButtonGradYear23.TabIndex = 2
+        Me.rButtonGradYear23.TabStop = True
+        Me.rButtonGradYear23.Text = "2023"
+        Me.rButtonGradYear23.UseVisualStyleBackColor = True
+        '
+        'rButtonGradYear22
+        '
+        Me.rButtonGradYear22.AutoSize = True
+        Me.rButtonGradYear22.Location = New System.Drawing.Point(85, 11)
+        Me.rButtonGradYear22.Name = "rButtonGradYear22"
+        Me.rButtonGradYear22.Size = New System.Drawing.Size(49, 17)
+        Me.rButtonGradYear22.TabIndex = 1
+        Me.rButtonGradYear22.TabStop = True
+        Me.rButtonGradYear22.Text = "2022"
+        Me.rButtonGradYear22.UseVisualStyleBackColor = True
+        '
+        'rButtonGradYear21
+        '
+        Me.rButtonGradYear21.AutoSize = True
+        Me.rButtonGradYear21.Location = New System.Drawing.Point(9, 11)
+        Me.rButtonGradYear21.Name = "rButtonGradYear21"
+        Me.rButtonGradYear21.Size = New System.Drawing.Size(49, 17)
+        Me.rButtonGradYear21.TabIndex = 0
+        Me.rButtonGradYear21.TabStop = True
+        Me.rButtonGradYear21.Text = "2021"
+        Me.rButtonGradYear21.UseVisualStyleBackColor = True
+        '
+        'groupBoxAge
+        '
+        Me.groupBoxAge.Controls.Add(Me.rButtonAge18)
+        Me.groupBoxAge.Controls.Add(Me.rButtonAge17)
+        Me.groupBoxAge.Controls.Add(Me.rButtonAge16)
+        Me.groupBoxAge.Controls.Add(Me.rButtonAge15)
+        Me.groupBoxAge.Location = New System.Drawing.Point(16, 96)
+        Me.groupBoxAge.Name = "groupBoxAge"
+        Me.groupBoxAge.Size = New System.Drawing.Size(96, 55)
+        Me.groupBoxAge.TabIndex = 13
+        Me.groupBoxAge.TabStop = False
+        '
+        'rButtonAge18
+        '
+        Me.rButtonAge18.AutoSize = True
+        Me.rButtonAge18.Location = New System.Drawing.Point(54, 32)
+        Me.rButtonAge18.Name = "rButtonAge18"
+        Me.rButtonAge18.Size = New System.Drawing.Size(37, 17)
+        Me.rButtonAge18.TabIndex = 3
+        Me.rButtonAge18.TabStop = True
+        Me.rButtonAge18.Text = "18"
+        Me.rButtonAge18.UseVisualStyleBackColor = True
+        '
+        'rButtonAge17
+        '
+        Me.rButtonAge17.AutoSize = True
+        Me.rButtonAge17.Location = New System.Drawing.Point(7, 34)
+        Me.rButtonAge17.Name = "rButtonAge17"
+        Me.rButtonAge17.Size = New System.Drawing.Size(37, 17)
+        Me.rButtonAge17.TabIndex = 2
+        Me.rButtonAge17.TabStop = True
+        Me.rButtonAge17.Text = "17"
+        Me.rButtonAge17.UseVisualStyleBackColor = True
+        '
+        'rButtonAge16
+        '
+        Me.rButtonAge16.AutoSize = True
+        Me.rButtonAge16.Location = New System.Drawing.Point(54, 11)
+        Me.rButtonAge16.Name = "rButtonAge16"
+        Me.rButtonAge16.Size = New System.Drawing.Size(37, 17)
+        Me.rButtonAge16.TabIndex = 1
+        Me.rButtonAge16.TabStop = True
+        Me.rButtonAge16.Text = "16"
+        Me.rButtonAge16.UseVisualStyleBackColor = True
+        '
+        'rButtonAge15
+        '
+        Me.rButtonAge15.AutoSize = True
+        Me.rButtonAge15.Location = New System.Drawing.Point(7, 11)
+        Me.rButtonAge15.Name = "rButtonAge15"
+        Me.rButtonAge15.Size = New System.Drawing.Size(37, 17)
+        Me.rButtonAge15.TabIndex = 0
+        Me.rButtonAge15.TabStop = True
+        Me.rButtonAge15.Text = "15"
+        Me.rButtonAge15.UseVisualStyleBackColor = True
+        '
+        'txtBoxFirstName
+        '
+        Me.txtBoxFirstName.Location = New System.Drawing.Point(16, 55)
+        Me.txtBoxFirstName.Name = "txtBoxFirstName"
+        Me.txtBoxFirstName.Size = New System.Drawing.Size(100, 20)
+        Me.txtBoxFirstName.TabIndex = 14
+        '
+        'txtBoxLastName
+        '
+        Me.txtBoxLastName.Location = New System.Drawing.Point(149, 56)
+        Me.txtBoxLastName.Name = "txtBoxLastName"
+        Me.txtBoxLastName.Size = New System.Drawing.Size(140, 20)
+        Me.txtBoxLastName.TabIndex = 15
+        '
+        'groupBoxGPA
+        '
+        Me.groupBoxGPA.Controls.Add(Me.rButtonGPA4)
+        Me.groupBoxGPA.Controls.Add(Me.rButtonGPA2)
+        Me.groupBoxGPA.Controls.Add(Me.rButtonGPA3)
+        Me.groupBoxGPA.Controls.Add(Me.rButtonGPA1)
+        Me.groupBoxGPA.Location = New System.Drawing.Point(330, 56)
+        Me.groupBoxGPA.Name = "groupBoxGPA"
+        Me.groupBoxGPA.Size = New System.Drawing.Size(58, 95)
+        Me.groupBoxGPA.TabIndex = 14
+        Me.groupBoxGPA.TabStop = False
+        '
+        'rButtonGPA4
+        '
+        Me.rButtonGPA4.AutoSize = True
+        Me.rButtonGPA4.Location = New System.Drawing.Point(9, 65)
+        Me.rButtonGPA4.Name = "rButtonGPA4"
+        Me.rButtonGPA4.Size = New System.Drawing.Size(40, 17)
+        Me.rButtonGPA4.TabIndex = 3
+        Me.rButtonGPA4.TabStop = True
+        Me.rButtonGPA4.Text = "4.0"
+        Me.rButtonGPA4.UseVisualStyleBackColor = True
+        '
+        'rButtonGPA2
+        '
+        Me.rButtonGPA2.AutoSize = True
+        Me.rButtonGPA2.Location = New System.Drawing.Point(9, 29)
+        Me.rButtonGPA2.Name = "rButtonGPA2"
+        Me.rButtonGPA2.Size = New System.Drawing.Size(40, 17)
+        Me.rButtonGPA2.TabIndex = 2
+        Me.rButtonGPA2.TabStop = True
+        Me.rButtonGPA2.Text = "2.0"
+        Me.rButtonGPA2.UseVisualStyleBackColor = True
+        '
+        'rButtonGPA3
+        '
+        Me.rButtonGPA3.AutoSize = True
+        Me.rButtonGPA3.Location = New System.Drawing.Point(9, 48)
+        Me.rButtonGPA3.Name = "rButtonGPA3"
+        Me.rButtonGPA3.Size = New System.Drawing.Size(40, 17)
+        Me.rButtonGPA3.TabIndex = 1
+        Me.rButtonGPA3.TabStop = True
+        Me.rButtonGPA3.Text = "3.0"
+        Me.rButtonGPA3.UseVisualStyleBackColor = True
+        '
+        'rButtonGPA1
+        '
+        Me.rButtonGPA1.AutoSize = True
+        Me.rButtonGPA1.Location = New System.Drawing.Point(9, 11)
+        Me.rButtonGPA1.Name = "rButtonGPA1"
+        Me.rButtonGPA1.Size = New System.Drawing.Size(40, 17)
+        Me.rButtonGPA1.TabIndex = 0
+        Me.rButtonGPA1.TabStop = True
+        Me.rButtonGPA1.Text = "1.0"
+        Me.rButtonGPA1.UseVisualStyleBackColor = True
+        '
+        'lblSearch
+        '
+        Me.lblSearch.AutoSize = True
+        Me.lblSearch.Location = New System.Drawing.Point(13, 194)
+        Me.lblSearch.Name = "lblSearch"
+        Me.lblSearch.Size = New System.Drawing.Size(44, 13)
+        Me.lblSearch.TabIndex = 16
+        Me.lblSearch.Text = "Search:"
+        '
+        'txtBoxSearch
+        '
+        Me.txtBoxSearch.Location = New System.Drawing.Point(16, 210)
+        Me.txtBoxSearch.Name = "txtBoxSearch"
+        Me.txtBoxSearch.Size = New System.Drawing.Size(375, 20)
+        Me.txtBoxSearch.TabIndex = 17
+        '
+        'timerDebugClearing
+        '
+        Me.timerDebugClearing.Enabled = True
+        Me.timerDebugClearing.Interval = 3000
+        '
+        'timerSearch
+        '
+        Me.timerSearch.Enabled = True
+        '
+        'frmStructureTest
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(799, 591)
+        Me.Controls.Add(Me.txtBoxSearch)
+        Me.Controls.Add(Me.lblSearch)
+        Me.Controls.Add(Me.groupBoxGPA)
+        Me.Controls.Add(Me.txtBoxLastName)
+        Me.Controls.Add(Me.txtBoxFirstName)
+        Me.Controls.Add(Me.groupBoxAge)
+        Me.Controls.Add(Me.groupBoxGradYear)
+        Me.Controls.Add(Me.lblGPA)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.listBoxResults)
+        Me.Controls.Add(Me.lblResults)
+        Me.Controls.Add(Me.listBoxDebugInformation)
+        Me.Controls.Add(Me.lblDebugInformation)
+        Me.Controls.Add(Me.lblGradYear)
+        Me.Controls.Add(Me.lblAge)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.lblStudentInfo)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmStructureTest"
+        Me.Text = "Student Structure Testing"
+        Me.groupBoxGradYear.ResumeLayout(False)
+        Me.groupBoxGradYear.PerformLayout()
+        Me.groupBoxAge.ResumeLayout(False)
+        Me.groupBoxAge.PerformLayout()
+        Me.groupBoxGPA.ResumeLayout(False)
+        Me.groupBoxGPA.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblStudentInfo As Label
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents lblAge As Label
+    Friend WithEvents lblGradYear As Label
+    Friend WithEvents lblDebugInformation As Label
+    Friend WithEvents listBoxDebugInformation As ListBox
+    Friend WithEvents lblResults As Label
+    Friend WithEvents listBoxResults As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents lblGPA As Label
+    Friend WithEvents groupBoxGradYear As GroupBox
+    Friend WithEvents groupBoxAge As GroupBox
+    Friend WithEvents txtBoxFirstName As TextBox
+    Friend WithEvents txtBoxLastName As TextBox
+    Friend WithEvents rButtonGradYear24 As RadioButton
+    Friend WithEvents rButtonGradYear23 As RadioButton
+    Friend WithEvents rButtonGradYear22 As RadioButton
+    Friend WithEvents rButtonGradYear21 As RadioButton
+    Friend WithEvents rButtonAge18 As RadioButton
+    Friend WithEvents rButtonAge17 As RadioButton
+    Friend WithEvents rButtonAge16 As RadioButton
+    Friend WithEvents rButtonAge15 As RadioButton
+    Friend WithEvents groupBoxGPA As GroupBox
+    Friend WithEvents rButtonGPA4 As RadioButton
+    Friend WithEvents rButtonGPA2 As RadioButton
+    Friend WithEvents rButtonGPA3 As RadioButton
+    Friend WithEvents rButtonGPA1 As RadioButton
+    Friend WithEvents lblSearch As Label
+    Friend WithEvents txtBoxSearch As TextBox
+    Friend WithEvents timerDebugClearing As Timer
+    Friend WithEvents timerSearch As Timer
+End Class
---
+++ Freetime/Structure Student Test/Structure Student Test/Form1.vb
@@ -0,0 +1,215 @@
+﻿' Created by kyematzen.com
+' Date completed on Nov 11. 2020
+
+' Custom object for storing individual student information.
+Public Structure Student
+    Dim firstName As String
+    Dim lastName As String
+    Dim age As Integer
+    Dim graduationYear As Decimal
+    Dim gradePointAverage As Double
+End Structure
+
+Public Class frmStructureTest
+
+    ' List of student objects used for storing information for all students.
+    Public Shared studentsList As New List(Of Student)
+
+    ' Cached search result.
+    Public Shared cachedSearchResult As String
+
+    ' Runs checks before adding student into studentsList.
+    Private Sub btnAddStudent(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        ' Steps:
+        ' 1. Is there a first name provided?
+        ' 2. Is there a last name provided?
+        ' 3. Is there a age selected?
+        ' 4. Is there a graduation year selected?
+        ' 5. Is there a GPA selected?
+
+        ' Step 1:
+
+        If txtBoxFirstName.Text = "" Then
+            logDebug("[S] No first name is entered.")
+            Return
+        End If
+
+        ' Step 2:
+
+        If txtBoxLastName.Text = "" Then
+            logDebug("[S] No last name is entered.")
+            Return
+        End If
+
+        ' Step 3:
+
+        If Not rButtonAge15.Checked And Not rButtonAge16.Checked And Not rButtonAge17.Checked And Not rButtonAge18.Checked Then
+            logDebug("[S] No age is selected.")
+            Return
+        End If
+
+        ' Step 4:
+
+        If Not rButtonGradYear21.Checked And Not rButtonGradYear22.Checked And Not rButtonGradYear23.Checked And Not rButtonGradYear24.Checked Then
+            logDebug("[S] No graduation year is selected.")
+            Return
+        End If
+
+        ' Step 5:
+
+        If Not rButtonGPA1.Checked And Not rButtonGPA2.Checked And Not rButtonGPA3.Checked And Not rButtonGPA4.Checked Then
+            logDebug("[S] No gpa is selected.")
+            Return
+        End If
+
+        Dim age As Integer = 0
+
+        If rButtonAge15.Checked Then
+            age = 15
+        ElseIf rButtonAge16.Checked Then
+            age = 16
+        ElseIf rButtonAge17.Checked Then
+            age = 17
+        Else
+            age = 18
+        End If
+
+        Dim gradYear As Integer = DateTime.Now.Year
+
+        If rButtonGradYear21.Checked Then
+            gradYear = 2021
+        ElseIf rButtonGradYear22.Checked Then
+            gradYear = 2022
+        ElseIf rButtonGradYear23.Checked Then
+            gradYear = 2023
+        Else
+            gradYear = 2024
+        End If
+
+        Dim gpa As Decimal = 0.0D
+
+        If rButtonGPA1.Checked Then
+            gpa = 1D
+        ElseIf rButtonGPA2.Checked Then
+            gpa = 2D
+        ElseIf rButtonGPA3.Checked Then
+            gpa = 3D
+        Else
+            gpa = 4D
+        End If
+
+        Dim student As Student
+
+        student.firstName = txtBoxFirstName.Text
+        student.lastName = txtBoxLastName.Text
+        student.age = age
+        student.graduationYear = gradYear
+        student.gradePointAverage = gpa
+
+        studentsList.Add(student)
+
+        logDebug("Student " & student.firstName & " " & student.lastName & " added to database.")
+
+        txtBoxFirstName.Text = ""
+        txtBoxLastName.Text = ""
+
+        For Each radioButton As RadioButton In groupBoxAge.Controls
+            radioButton.Checked = False
+        Next
+
+        For Each radioButton As RadioButton In groupBoxGradYear.Controls
+            radioButton.Checked = False
+        Next
+
+        For Each radioButton As RadioButton In groupBoxGPA.Controls
+            radioButton.Checked = False
+        Next
+    End Sub
+
+    ' Clears debug console every 3s if there are more than 10 lines.
+    Private Sub timerDebugClear(sender As Object, e As EventArgs) Handles timerDebugClearing.Tick
+
+        If listBoxDebugInformation.Items.Count >= 15 Then
+            listBoxDebugInformation.Items.RemoveAt(0)
+            listBoxDebugInformation.Update()
+
+            If timerDebugClearing.Interval > 250 Then
+                timerDebugClearing.Interval = timerDebugClearing.Interval - 150
+
+                Console.WriteLine(timerDebugClearing.Interval)
+            End If
+        Else
+            timerDebugClearing.Interval = 3000
+        End If
+
+    End Sub
+
+    ' Logs information into a local console of the program.
+    Sub logDebug(ByRef info As String)
+        Dim consoleSize As Integer = listBoxDebugInformation.Items.Count
+
+        ' Checks if the previous message is the same, before adding to prevent spam.
+        If consoleSize > 0 AndAlso listBoxDebugInformation.Items(consoleSize - 1).ToString.Contains(info) Then
+            Return
+        End If
+
+        listBoxDebugInformation.Items.Add(DateTime.Now.ToShortTimeString & ": " & info)
+    End Sub
+
+    ' Updates database information sorting objects based on names.
+    Private Sub timerSearch_Tick(sender As Object, e As EventArgs) Handles timerSearch.Tick
+        If studentsList.Count = 0 Then
+            logDebug("No entries found.")
+            Return
+        End If
+
+        ' Steps:
+        ' 1. Check cache and see if search has changed, if not return.
+        '
+        ' 2. Check if search bar is empty.
+        ' 2.1 If empty, show all students after sorting by first name.
+        ' 2.2 If not empty, check if it is one of the common phrases.
+        ' 2.3 If not a common phrase, return "No results found".
+        '
+        ' 3. Determine output of students dependent on common phrase
+        '
+        ' 3.1 Check if common phrase starts with "gpa"
+        ' 3.1.1 Remove anything else other than decimal number.
+        ' 3.1.1.1 If no decimal is found, return "No results found".
+        ' 3.1.1.2 If decimal is found, find GPAs that are greater than or equal to requested GPA.
+        '
+        ' 3.2 Check if common phrase starts with "age"
+        ' 3.2.1 Remove anything else other than number.
+        ' 3.2.1.1 If no number is found, return "No results found".
+        ' 3.2.1.2 If number is valid age, find students that are at that age.
+        '
+        ' 3.3 Check if common phrase starts with "graduation year", or "grad year"
+        ' 3.3.1 Remove anything else other than graduation year.
+        ' 3.3.1.1 If no graduation year is found, return "No results found".
+        ' 3.3.1.2 If graduation year is found, find students with that graduation year.
+        '
+        '
+        ' 3.4 If none of the common phrases were found, check first/last names.
+        ' 3.4.1 If there are first names but no last names with result, return in a format of
+        '
+        ' Students with first name of Result:
+        ' Kyle Matzen (other info)
+        '
+        ' 3.4.2 If there are first names and last names with result, return in a format of
+        '
+        ' Students with first name of Result:
+        ' Kyle Matzen (other info)
+        '
+        ' Students with last name of Result:
+        ' Matzen Kyle (other info)
+        '
+        ' 3.4.3 If there are last names but no first names with result, return in a format of
+        ' 
+        ' Students with last name of Result:
+        ' Matzen Kyle (other info)
+        '
+        ' 3.4.4 If there are no first/last names with result, return "No results found."
+
+    End Sub
+End Class
