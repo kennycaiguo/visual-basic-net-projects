bf464a0cde91edd569acf96e4fae581c9cd450ef
kyematzen <kyleofficalhd@gmail.com>
2020-11-12 19:03:45 -0500

    Added new project Click Handler

---
+++ Freetime/Click Handler/Click Handler/Form1.vb
@@ -0,0 +1,21 @@
+﻿' Created by kyematzen.com
+' Date completed on Nov 12. 2020
+Public Class frmClickHandler
+
+    Public Sub ClickHandler(sender As Object, e As MouseEventArgs) Handles Me.MouseClick
+        Me.Text = "You just clicked with " & getButton(e.Button) & " at position " & MousePosition().ToString
+    End Sub
+
+    Function getButton(ByRef mouseButton As MouseButtons) As String
+        Select Case mouseButton
+            Case MouseButtons.Left
+                Return "left button"
+            Case MouseButtons.Right
+                Return "right button"
+            Case MouseButtons.Middle
+                Return "middle button"
+        End Select
+
+        Return "another mouse button"
+    End Function
+End Class
---
+++ Freetime/Click Handler/Click Handler/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Click Handler")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Click Handler")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3290ba08-976f-4792-a7a8-9f82d2d71c33")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
