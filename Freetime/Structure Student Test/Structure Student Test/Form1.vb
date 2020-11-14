8df697fa35998723a51709c0609f9e14f4f091a1
kyematzen <kyleofficalhd@gmail.com>
2020-11-14 17:14:11 -0500

    Added license

--- Freetime/Structure Student Test/Structure Student Test/Form1.vb
+++ Freetime/Structure Student Test/Structure Student Test/Form1.vb
@@ -1,7 +1,19 @@
-﻿' Created by kyematzen.com
-' Date completed on Nov 11. 2020
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
 
-' Custom object for storing individual student information.
 Public Structure Student
     Dim firstName As String
     Dim lastName As String
@@ -314,5 +326,9 @@ Public Class frmStructureTest
     Private Sub frmStructureTest_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         dataGridViewResults.BackgroundColor = Color.White
         dataGridViewResults.RowHeadersVisible = False
+    End Sub
+
+    Private Sub dataGridViewResults_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dataGridViewResults.CellContentClick
+
     End Sub
 End Class
