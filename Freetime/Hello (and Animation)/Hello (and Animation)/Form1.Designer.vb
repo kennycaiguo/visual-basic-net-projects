0b1adefc6c23b8f17599dd50a855b3ab5985d88a
kyematzen <kyleofficalhd@gmail.com>
2020-11-14 17:16:35 -0500

    Added license

--- Freetime/Hello (and Animation)/Hello (and Animation)/Form1.Designer.vb
+++ Freetime/Hello (and Animation)/Hello (and Animation)/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmHelloWithAnimation
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -23,7 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmHelloWithAnimation))
         Me.groupBoxColor = New System.Windows.Forms.GroupBox()
         Me.rButtonColorRandom = New System.Windows.Forms.RadioButton()
         Me.rButtonColorAqua = New System.Windows.Forms.RadioButton()
@@ -353,7 +353,7 @@ Partial Class Form1
         Me.animateColor.Interval = 150
         Me.animateColor.Tag = ""
         '
-        'Form1
+        'frmHelloWithAnimation
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
@@ -366,7 +366,7 @@ Partial Class Form1
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
-        Me.Name = "Form1"
+        Me.Name = "frmHelloWithAnimation"
         Me.ShowIcon = False
         Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
         Me.Text = "Hello (with Animation)"
