7ebca0b9e1a108cf731624756fc772e3be604641
kyematzen <kyleofficalhd@gmail.com>
2020-11-14 19:53:51 -0500

    Updated project Railroad Properties

--- Book/Railraod Properties/Railraod Properties/Form1.vb
+++ Book/Railraod Properties/Railraod Properties/Form1.vb
@@ -1,8 +1,29 @@
-﻿Public Class frmMonopoly
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class frmMonopoly
+    ' Fires when user selects a different property
     Private Sub selectedProperty(sender As Object, e As EventArgs) Handles listBoxProperties.SelectedIndexChanged
+        ' Checks if user selected a propery or nothing
         If Not listBoxProperties.SelectedIndex = -1 Then
+            ' Grabs name of item selected
             Dim name As String = listBoxProperties.Items(listBoxProperties.SelectedIndex)
 
+            ' Loops through responses
+            ' If the name is Short Line, they got it correct, else wrong.
             Select Case name
                 Case "Short Line"
                     MessageBox.Show("Short Line is a bus company.", "Correct")
