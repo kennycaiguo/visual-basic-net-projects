9be68d0a949ad808489216251bc40a4a88df4825
kyematzen <kyleofficalhd@gmail.com>
2020-11-14 20:55:37 -0500

    Added new project Eight Billion Population

---
+++ Book/Eight Billion Population/Eight Billion Population/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPopulation
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDetermine = New System.Windows.Forms.Button()
+        Me.txtBoxCalculation = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnDetermine
+        '
+        Me.btnDetermine.Location = New System.Drawing.Point(12, 12)
+        Me.btnDetermine.Name = "btnDetermine"
+        Me.btnDetermine.Size = New System.Drawing.Size(262, 23)
+        Me.btnDetermine.TabIndex = 0
+        Me.btnDetermine.Text = "Determine When Population Is 8 Billion"
+        Me.btnDetermine.UseVisualStyleBackColor = True
+        '
+        'txtBoxCalculation
+        '
+        Me.txtBoxCalculation.Location = New System.Drawing.Point(13, 42)
+        Me.txtBoxCalculation.Name = "txtBoxCalculation"
+        Me.txtBoxCalculation.ReadOnly = True
+        Me.txtBoxCalculation.Size = New System.Drawing.Size(261, 20)
+        Me.txtBoxCalculation.TabIndex = 1
+        '
+        'frmPopulation
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(286, 74)
+        Me.Controls.Add(Me.txtBoxCalculation)
+        Me.Controls.Add(Me.btnDetermine)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmPopulation"
+        Me.Text = "Population"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDetermine As Button
+    Friend WithEvents txtBoxCalculation As TextBox
+End Class
---
+++ Book/Eight Billion Population/Eight Billion Population/Form1.vb
@@ -0,0 +1,30 @@
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class frmPopulation
+    Private Sub btnDeterminePopulation(sender As Object, e As EventArgs) Handles btnDetermine.Click
+        Dim population As Double = 7000000000D
+        Dim year As Integer = 2011
+
+        While (population < 8000000000D)
+            population += (population * 0.011)
+            year += 1
+        End While
+
+        txtBoxCalculation.Text = "World population will reach 8 billion in " & year & "."
+
+    End Sub
+End Class
