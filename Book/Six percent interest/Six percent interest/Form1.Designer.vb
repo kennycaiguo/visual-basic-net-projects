b161dc730b74a977a689837b6c3614c00d6edffa
kyematzen <kyleofficalhd@gmail.com>
2020-11-14 20:10:19 -0500

    Added new project Six percent interest

---
+++ Book/Six percent interest/Six percent interest/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMillionare
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblAmount = New System.Windows.Forms.Label()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.txtWhen = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblAmount
+        '
+        Me.lblAmount.AutoSize = True
+        Me.lblAmount.Location = New System.Drawing.Point(12, 13)
+        Me.lblAmount.Name = "lblAmount"
+        Me.lblAmount.Size = New System.Drawing.Size(95, 13)
+        Me.lblAmount.TabIndex = 0
+        Me.lblAmount.Text = "Amount deposited:"
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(113, 10)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(100, 20)
+        Me.txtAmount.TabIndex = 1
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(13, 39)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(201, 23)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate Years to Become a Millionare"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'txtWhen
+        '
+        Me.txtWhen.Location = New System.Drawing.Point(15, 69)
+        Me.txtWhen.Name = "txtWhen"
+        Me.txtWhen.ReadOnly = True
+        Me.txtWhen.Size = New System.Drawing.Size(198, 20)
+        Me.txtWhen.TabIndex = 3
+        '
+        'frmMillionare
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(226, 95)
+        Me.Controls.Add(Me.txtWhen)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.lblAmount)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMillionare"
+        Me.Text = "6% Interest"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblAmount As Label
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents txtWhen As TextBox
+End Class
---
+++ Book/Six percent interest/Six percent interest/Form1.vb
@@ -0,0 +1,33 @@
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class frmMillionare
+    ' Method used to compute years until a millionare
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Compute years required to become a millionare
+        Dim balance As Decimal, numYears As Integer = 0
+        balance = CDec(txtAmount.Text)
+
+        ' Until balance is over 1,000,000 continue to multiply current balance by 6% and add that value to balance, and increase years by one.
+        Do While balance < 1000000
+            balance += 0.06D * balance
+            numYears += 1
+        Loop
+
+        ' Updates text mentioning how many years until you will have a million dollars.
+        txtWhen.Text = "In " & numYears & " years you will have a million dollars."
+    End Sub
+End Class
