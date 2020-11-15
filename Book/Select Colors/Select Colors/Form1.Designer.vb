7a9366fb931c894491b4c45ad164457f18e37e3f
kyematzen <kyleofficalhd@gmail.com>
2020-11-14 19:30:31 -0500

    Added new project Select Colors

---
+++ Book/Select Colors/Select Colors/Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSelectColors
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.groupBoxForeground = New System.Windows.Forms.GroupBox()
+        Me.rButtonForegroundBlue = New System.Windows.Forms.RadioButton()
+        Me.rButtonForegroundRed = New System.Windows.Forms.RadioButton()
+        Me.rButtonForegroundYellow = New System.Windows.Forms.RadioButton()
+        Me.rButtonForegroundWhite = New System.Windows.Forms.RadioButton()
+        Me.groupBoxBackground = New System.Windows.Forms.GroupBox()
+        Me.rButtonBackgroundBlue = New System.Windows.Forms.RadioButton()
+        Me.rButtonBackgroundRed = New System.Windows.Forms.RadioButton()
+        Me.rButtonBackgroundYellow = New System.Windows.Forms.RadioButton()
+        Me.rButtonBackgroundWhite = New System.Windows.Forms.RadioButton()
+        Me.btnApply = New System.Windows.Forms.Button()
+        Me.lblVisualBasic = New System.Windows.Forms.Label()
+        Me.groupBoxForeground.SuspendLayout()
+        Me.groupBoxBackground.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'groupBoxForeground
+        '
+        Me.groupBoxForeground.Controls.Add(Me.rButtonForegroundBlue)
+        Me.groupBoxForeground.Controls.Add(Me.rButtonForegroundRed)
+        Me.groupBoxForeground.Controls.Add(Me.rButtonForegroundYellow)
+        Me.groupBoxForeground.Controls.Add(Me.rButtonForegroundWhite)
+        Me.groupBoxForeground.Location = New System.Drawing.Point(13, 13)
+        Me.groupBoxForeground.Name = "groupBoxForeground"
+        Me.groupBoxForeground.Size = New System.Drawing.Size(120, 121)
+        Me.groupBoxForeground.TabIndex = 0
+        Me.groupBoxForeground.TabStop = False
+        Me.groupBoxForeground.Text = "Foreground Color"
+        '
+        'rButtonForegroundBlue
+        '
+        Me.rButtonForegroundBlue.AutoSize = True
+        Me.rButtonForegroundBlue.Location = New System.Drawing.Point(7, 89)
+        Me.rButtonForegroundBlue.Name = "rButtonForegroundBlue"
+        Me.rButtonForegroundBlue.Size = New System.Drawing.Size(46, 17)
+        Me.rButtonForegroundBlue.TabIndex = 3
+        Me.rButtonForegroundBlue.Text = "Blue"
+        Me.rButtonForegroundBlue.UseVisualStyleBackColor = True
+        '
+        'rButtonForegroundRed
+        '
+        Me.rButtonForegroundRed.AutoSize = True
+        Me.rButtonForegroundRed.Location = New System.Drawing.Point(7, 66)
+        Me.rButtonForegroundRed.Name = "rButtonForegroundRed"
+        Me.rButtonForegroundRed.Size = New System.Drawing.Size(45, 17)
+        Me.rButtonForegroundRed.TabIndex = 2
+        Me.rButtonForegroundRed.Text = "Red"
+        Me.rButtonForegroundRed.UseVisualStyleBackColor = True
+        '
+        'rButtonForegroundYellow
+        '
+        Me.rButtonForegroundYellow.AutoSize = True
+        Me.rButtonForegroundYellow.Location = New System.Drawing.Point(7, 43)
+        Me.rButtonForegroundYellow.Name = "rButtonForegroundYellow"
+        Me.rButtonForegroundYellow.Size = New System.Drawing.Size(56, 17)
+        Me.rButtonForegroundYellow.TabIndex = 1
+        Me.rButtonForegroundYellow.Text = "Yellow"
+        Me.rButtonForegroundYellow.UseVisualStyleBackColor = True
+        '
+        'rButtonForegroundWhite
+        '
+        Me.rButtonForegroundWhite.AutoSize = True
+        Me.rButtonForegroundWhite.Location = New System.Drawing.Point(7, 20)
+        Me.rButtonForegroundWhite.Name = "rButtonForegroundWhite"
+        Me.rButtonForegroundWhite.Size = New System.Drawing.Size(53, 17)
+        Me.rButtonForegroundWhite.TabIndex = 0
+        Me.rButtonForegroundWhite.Text = "White"
+        Me.rButtonForegroundWhite.UseVisualStyleBackColor = True
+        '
+        'groupBoxBackground
+        '
+        Me.groupBoxBackground.Controls.Add(Me.rButtonBackgroundBlue)
+        Me.groupBoxBackground.Controls.Add(Me.rButtonBackgroundRed)
+        Me.groupBoxBackground.Controls.Add(Me.rButtonBackgroundYellow)
+        Me.groupBoxBackground.Controls.Add(Me.rButtonBackgroundWhite)
+        Me.groupBoxBackground.Location = New System.Drawing.Point(139, 13)
+        Me.groupBoxBackground.Name = "groupBoxBackground"
+        Me.groupBoxBackground.Size = New System.Drawing.Size(120, 121)
+        Me.groupBoxBackground.TabIndex = 4
+        Me.groupBoxBackground.TabStop = False
+        Me.groupBoxBackground.Text = "Background Color"
+        '
+        'rButtonBackgroundBlue
+        '
+        Me.rButtonBackgroundBlue.AutoSize = True
+        Me.rButtonBackgroundBlue.Location = New System.Drawing.Point(7, 89)
+        Me.rButtonBackgroundBlue.Name = "rButtonBackgroundBlue"
+        Me.rButtonBackgroundBlue.Size = New System.Drawing.Size(46, 17)
+        Me.rButtonBackgroundBlue.TabIndex = 3
+        Me.rButtonBackgroundBlue.Text = "Blue"
+        Me.rButtonBackgroundBlue.UseVisualStyleBackColor = True
+        '
+        'rButtonBackgroundRed
+        '
+        Me.rButtonBackgroundRed.AutoSize = True
+        Me.rButtonBackgroundRed.Location = New System.Drawing.Point(7, 66)
+        Me.rButtonBackgroundRed.Name = "rButtonBackgroundRed"
+        Me.rButtonBackgroundRed.Size = New System.Drawing.Size(45, 17)
+        Me.rButtonBackgroundRed.TabIndex = 2
+        Me.rButtonBackgroundRed.Text = "Red"
+        Me.rButtonBackgroundRed.UseVisualStyleBackColor = True
+        '
+        'rButtonBackgroundYellow
+        '
+        Me.rButtonBackgroundYellow.AutoSize = True
+        Me.rButtonBackgroundYellow.Location = New System.Drawing.Point(7, 43)
+        Me.rButtonBackgroundYellow.Name = "rButtonBackgroundYellow"
+        Me.rButtonBackgroundYellow.Size = New System.Drawing.Size(56, 17)
+        Me.rButtonBackgroundYellow.TabIndex = 1
+        Me.rButtonBackgroundYellow.Text = "Yellow"
+        Me.rButtonBackgroundYellow.UseVisualStyleBackColor = True
+        '
+        'rButtonBackgroundWhite
+        '
+        Me.rButtonBackgroundWhite.AutoSize = True
+        Me.rButtonBackgroundWhite.Location = New System.Drawing.Point(7, 20)
+        Me.rButtonBackgroundWhite.Name = "rButtonBackgroundWhite"
+        Me.rButtonBackgroundWhite.Size = New System.Drawing.Size(53, 17)
+        Me.rButtonBackgroundWhite.TabIndex = 0
+        Me.rButtonBackgroundWhite.Text = "White"
+        Me.rButtonBackgroundWhite.UseVisualStyleBackColor = True
+        '
+        'btnApply
+        '
+        Me.btnApply.Location = New System.Drawing.Point(308, 56)
+        Me.btnApply.Name = "btnApply"
+        Me.btnApply.Size = New System.Drawing.Size(75, 48)
+        Me.btnApply.TabIndex = 5
+        Me.btnApply.Text = "Apply" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Colors"
+        Me.btnApply.UseVisualStyleBackColor = True
+        '
+        'lblVisualBasic
+        '
+        Me.lblVisualBasic.Font = New System.Drawing.Font("Microsoft Sans Serif", 35.0!)
+        Me.lblVisualBasic.Location = New System.Drawing.Point(12, 146)
+        Me.lblVisualBasic.Name = "lblVisualBasic"
+        Me.lblVisualBasic.Size = New System.Drawing.Size(408, 105)
+        Me.lblVisualBasic.TabIndex = 6
+        Me.lblVisualBasic.Text = "VISUAL BASIC"
+        Me.lblVisualBasic.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmSelectColors
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(432, 260)
+        Me.Controls.Add(Me.lblVisualBasic)
+        Me.Controls.Add(Me.btnApply)
+        Me.Controls.Add(Me.groupBoxBackground)
+        Me.Controls.Add(Me.groupBoxForeground)
+        Me.Name = "frmSelectColors"
+        Me.Text = "Colors"
+        Me.groupBoxForeground.ResumeLayout(False)
+        Me.groupBoxForeground.PerformLayout()
+        Me.groupBoxBackground.ResumeLayout(False)
+        Me.groupBoxBackground.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents groupBoxForeground As GroupBox
+    Friend WithEvents rButtonForegroundBlue As RadioButton
+    Friend WithEvents rButtonForegroundRed As RadioButton
+    Friend WithEvents rButtonForegroundYellow As RadioButton
+    Friend WithEvents rButtonForegroundWhite As RadioButton
+    Friend WithEvents groupBoxBackground As GroupBox
+    Friend WithEvents rButtonBackgroundBlue As RadioButton
+    Friend WithEvents rButtonBackgroundRed As RadioButton
+    Friend WithEvents rButtonBackgroundYellow As RadioButton
+    Friend WithEvents rButtonBackgroundWhite As RadioButton
+    Friend WithEvents btnApply As Button
+    Friend WithEvents lblVisualBasic As Label
+End Class
---
+++ Book/Select Colors/Select Colors/Form1.vb
@@ -0,0 +1,64 @@
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class frmSelectColors
+    Private Sub btnApply_Click(sender As Object, e As EventArgs) Handles btnApply.Click
+        ' Checks if a foreground color was selected.
+        If Not rButtonForegroundWhite.Checked AndAlso Not rButtonForegroundYellow.Checked AndAlso Not rButtonForegroundRed.Checked AndAlso Not rButtonForegroundBlue.Checked Then
+            MessageBox.Show("You must provide a foreground color", "Colors")
+            Return
+        End If
+
+        ' Check if a background color was selected.
+        If Not rButtonBackgroundWhite.Checked AndAlso Not rButtonBackgroundYellow.Checked AndAlso Not rButtonBackgroundRed.Checked AndAlso Not rButtonBackgroundBlue.Checked Then
+            MessageBox.Show("You must provide a background color", "Colors")
+            Return
+        End If
+
+        ' Finds what color user wants as foreground.
+        Dim foreColor As Color = Color.Blue
+
+        If rButtonForegroundWhite.Checked Then
+            foreColor = Color.White
+        ElseIf rButtonForegroundYellow.Checked Then
+            foreColor = Color.Yellow
+        ElseIf rButtonForegroundRed.Checked Then
+            foreColor = Color.Red
+        End If
+
+        ' Finds what color user wants as background.
+        Dim backColor As Color = Color.Blue
+
+        If rButtonBackgroundWhite.Checked Then
+            backColor = Color.White
+        ElseIf rButtonBackgroundYellow.Checked Then
+            backColor = Color.Yellow
+        ElseIf rButtonBackgroundRed.Checked Then
+            backColor = Color.Red
+        End If
+
+        ' Checks if foreColor and backColor are the same.
+        If foreColor = backColor Then
+            MessageBox.Show("Foreground and Background color cannot both be " & foreColor.Name.ToString.ToUpper & ".", "Colors")
+            Return
+        End If
+
+        ' Updates text foreground and background color for lblVisualBasic
+
+        lblVisualBasic.ForeColor = foreColor
+        lblVisualBasic.BackColor = backColor
+    End Sub
+End Class
