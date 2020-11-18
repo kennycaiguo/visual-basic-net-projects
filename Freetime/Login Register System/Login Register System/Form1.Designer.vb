39d3a43c6f4c8f2db454fc56aecc9cd1dfdc6f54
kyematzen <kyleofficalhd@gmail.com>
2020-11-18 18:30:44 -0500

    Added new project Login Register System

---
+++ Freetime/Login Register System/Login Register System/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLoginRegisterSystem
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblUsername
+        '
+        Me.lblUsername.Location = New System.Drawing.Point(12, 9)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(221, 257)
+        Me.lblUsername.TabIndex = 0
+        Me.lblUsername.Text = "Username:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Password:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.lblUsername.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'frmLoginRegisterSystem
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(245, 275)
+        Me.Controls.Add(Me.lblUsername)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmLoginRegisterSystem"
+        Me.Text = "Login Form"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lblUsername As Label
+End Class
---
+++ Freetime/Login Register System/Login Register System/Form1.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmLoginRegisterSystem
+
+End Class
