b23851fbecbff98295edf79603b9c68374afdf32
kyematzen <kyleofficalhd@gmail.com>
2020-11-18 19:32:39 -0500

    Updated project Login Register System

--- Freetime/Login Register System/Login Register System/Form1.vb
+++ Freetime/Login Register System/Login Register System/Form1.vb
@@ -1,5 +1,22 @@
-﻿Public Class frmLoginRegisterSystem
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class frmLoginRegisterSystem
     Private Sub lblOption_Click(sender As Object, e As EventArgs) Handles lblOption.Click
+
         If lblOption.Text = "click here to create a new account" Then
             lblOption.Text = "click here to login"
 
@@ -42,4 +59,16 @@
         txtBoxRegisterPassword.Visible = False
         txtBoxRegisterRetypePassword.Visible = False
     End Sub
+
+    Private Sub loginForm(sender As Object, e As EventArgs) Handles btnLogin.Click
+        ' No users in database and should return
+        If Not IO.File.Exists("database.txt") Then
+            Return
+        End If
+    End Sub
+
+    Private Sub registerForm(sender As Object, e As EventArgs) Handles btnRegister.Click
+
+    End Sub
+
 End Class
