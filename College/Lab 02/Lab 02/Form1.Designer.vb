de7dc47d6954d921a6805bdb4304edf51f1e6eb5
kyematzen <kyleofficalhd@gmail.com>
2020-11-29 13:02:05 -0500

    Updated project Lab 02

--- College/Lab 02/Lab 02/Form1.Designer.vb
+++ College/Lab 02/Lab 02/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmAirplaneData
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,17 +22,199 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnComputeFuel = New System.Windows.Forms.Button()
+        Me.dataGridFuel = New System.Windows.Forms.DataGridView()
+        Me.btnAirplaneDetails = New System.Windows.Forms.Button()
+        Me.lblAirplaneModelDesign = New System.Windows.Forms.Label()
+        Me.lblNumberOfSeatsDesign = New System.Windows.Forms.Label()
+        Me.lblSectorDesign = New System.Windows.Forms.Label()
+        Me.lblFuelBurnDesign = New System.Windows.Forms.Label()
+        Me.lblFuelPerSeatDesign = New System.Windows.Forms.Label()
+        Me.lblRoundTripFuelDesign = New System.Windows.Forms.Label()
+        Me.lblAirplaneModel = New System.Windows.Forms.Label()
+        Me.lblNumberOfSeats = New System.Windows.Forms.Label()
+        Me.lblSector = New System.Windows.Forms.Label()
+        Me.lblFuelBurn = New System.Windows.Forms.Label()
+        Me.lblFuelPerSeat = New System.Windows.Forms.Label()
+        Me.lblRoundTripFuel = New System.Windows.Forms.Label()
+        CType(Me.dataGridFuel, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Form1
+        'btnComputeFuel
+        '
+        Me.btnComputeFuel.Location = New System.Drawing.Point(67, 10)
+        Me.btnComputeFuel.Name = "btnComputeFuel"
+        Me.btnComputeFuel.Size = New System.Drawing.Size(95, 23)
+        Me.btnComputeFuel.TabIndex = 0
+        Me.btnComputeFuel.Text = "Compute Fuel"
+        Me.btnComputeFuel.UseVisualStyleBackColor = True
+        '
+        'dataGridFuel
+        '
+        Me.dataGridFuel.AllowUserToResizeColumns = False
+        Me.dataGridFuel.AllowUserToResizeRows = False
+        Me.dataGridFuel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataGridFuel.Location = New System.Drawing.Point(13, 36)
+        Me.dataGridFuel.Name = "dataGridFuel"
+        Me.dataGridFuel.ReadOnly = True
+        Me.dataGridFuel.Size = New System.Drawing.Size(203, 212)
+        Me.dataGridFuel.TabIndex = 1
+        '
+        'btnAirplaneDetails
+        '
+        Me.btnAirplaneDetails.Location = New System.Drawing.Point(292, 10)
+        Me.btnAirplaneDetails.Name = "btnAirplaneDetails"
+        Me.btnAirplaneDetails.Size = New System.Drawing.Size(95, 23)
+        Me.btnAirplaneDetails.TabIndex = 2
+        Me.btnAirplaneDetails.Text = "Airplane Details"
+        Me.btnAirplaneDetails.UseVisualStyleBackColor = True
+        '
+        'lblAirplaneModelDesign
+        '
+        Me.lblAirplaneModelDesign.AutoSize = True
+        Me.lblAirplaneModelDesign.Location = New System.Drawing.Point(222, 73)
+        Me.lblAirplaneModelDesign.Name = "lblAirplaneModelDesign"
+        Me.lblAirplaneModelDesign.Size = New System.Drawing.Size(80, 13)
+        Me.lblAirplaneModelDesign.TabIndex = 3
+        Me.lblAirplaneModelDesign.Text = "Airplane Model:"
+        '
+        'lblNumberOfSeatsDesign
+        '
+        Me.lblNumberOfSeatsDesign.AutoSize = True
+        Me.lblNumberOfSeatsDesign.Location = New System.Drawing.Point(222, 97)
+        Me.lblNumberOfSeatsDesign.Name = "lblNumberOfSeatsDesign"
+        Me.lblNumberOfSeatsDesign.Size = New System.Drawing.Size(89, 13)
+        Me.lblNumberOfSeatsDesign.TabIndex = 4
+        Me.lblNumberOfSeatsDesign.Text = "Number of Seats:"
+        '
+        'lblSectorDesign
+        '
+        Me.lblSectorDesign.AutoSize = True
+        Me.lblSectorDesign.Location = New System.Drawing.Point(222, 121)
+        Me.lblSectorDesign.Name = "lblSectorDesign"
+        Me.lblSectorDesign.Size = New System.Drawing.Size(41, 13)
+        Me.lblSectorDesign.TabIndex = 5
+        Me.lblSectorDesign.Text = "Sector:"
+        '
+        'lblFuelBurnDesign
+        '
+        Me.lblFuelBurnDesign.AutoSize = True
+        Me.lblFuelBurnDesign.Location = New System.Drawing.Point(222, 146)
+        Me.lblFuelBurnDesign.Name = "lblFuelBurnDesign"
+        Me.lblFuelBurnDesign.Size = New System.Drawing.Size(55, 13)
+        Me.lblFuelBurnDesign.TabIndex = 6
+        Me.lblFuelBurnDesign.Text = "Fuel Burn:"
+        '
+        'lblFuelPerSeatDesign
+        '
+        Me.lblFuelPerSeatDesign.AutoSize = True
+        Me.lblFuelPerSeatDesign.Location = New System.Drawing.Point(222, 171)
+        Me.lblFuelPerSeatDesign.Name = "lblFuelPerSeatDesign"
+        Me.lblFuelPerSeatDesign.Size = New System.Drawing.Size(74, 13)
+        Me.lblFuelPerSeatDesign.TabIndex = 7
+        Me.lblFuelPerSeatDesign.Text = "Fuel Per Seat:"
+        '
+        'lblRoundTripFuelDesign
+        '
+        Me.lblRoundTripFuelDesign.AutoSize = True
+        Me.lblRoundTripFuelDesign.Location = New System.Drawing.Point(222, 193)
+        Me.lblRoundTripFuelDesign.Name = "lblRoundTripFuelDesign"
+        Me.lblRoundTripFuelDesign.Size = New System.Drawing.Size(113, 13)
+        Me.lblRoundTripFuelDesign.TabIndex = 8
+        Me.lblRoundTripFuelDesign.Text = "Total Round Trip Fuel:"
+        '
+        'lblAirplaneModel
+        '
+        Me.lblAirplaneModel.Location = New System.Drawing.Point(338, 73)
+        Me.lblAirplaneModel.Name = "lblAirplaneModel"
+        Me.lblAirplaneModel.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.lblAirplaneModel.Size = New System.Drawing.Size(105, 13)
+        Me.lblAirplaneModel.TabIndex = 9
+        '
+        'lblNumberOfSeats
+        '
+        Me.lblNumberOfSeats.Location = New System.Drawing.Point(338, 97)
+        Me.lblNumberOfSeats.Name = "lblNumberOfSeats"
+        Me.lblNumberOfSeats.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.lblNumberOfSeats.Size = New System.Drawing.Size(102, 13)
+        Me.lblNumberOfSeats.TabIndex = 10
+        '
+        'lblSector
+        '
+        Me.lblSector.Location = New System.Drawing.Point(338, 121)
+        Me.lblSector.Name = "lblSector"
+        Me.lblSector.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.lblSector.Size = New System.Drawing.Size(102, 13)
+        Me.lblSector.TabIndex = 11
+        '
+        'lblFuelBurn
+        '
+        Me.lblFuelBurn.Location = New System.Drawing.Point(338, 146)
+        Me.lblFuelBurn.Name = "lblFuelBurn"
+        Me.lblFuelBurn.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.lblFuelBurn.Size = New System.Drawing.Size(102, 13)
+        Me.lblFuelBurn.TabIndex = 12
+        '
+        'lblFuelPerSeat
+        '
+        Me.lblFuelPerSeat.Location = New System.Drawing.Point(338, 171)
+        Me.lblFuelPerSeat.Name = "lblFuelPerSeat"
+        Me.lblFuelPerSeat.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.lblFuelPerSeat.Size = New System.Drawing.Size(102, 13)
+        Me.lblFuelPerSeat.TabIndex = 13
+        '
+        'lblRoundTripFuel
+        '
+        Me.lblRoundTripFuel.Location = New System.Drawing.Point(338, 193)
+        Me.lblRoundTripFuel.Name = "lblRoundTripFuel"
+        Me.lblRoundTripFuel.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.lblRoundTripFuel.Size = New System.Drawing.Size(102, 13)
+        Me.lblRoundTripFuel.TabIndex = 14
+        '
+        'frmAirplaneData
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(455, 257)
+        Me.Controls.Add(Me.lblRoundTripFuel)
+        Me.Controls.Add(Me.lblFuelPerSeat)
+        Me.Controls.Add(Me.lblFuelBurn)
+        Me.Controls.Add(Me.lblSector)
+        Me.Controls.Add(Me.lblNumberOfSeats)
+        Me.Controls.Add(Me.lblAirplaneModel)
+        Me.Controls.Add(Me.lblRoundTripFuelDesign)
+        Me.Controls.Add(Me.lblFuelPerSeatDesign)
+        Me.Controls.Add(Me.lblFuelBurnDesign)
+        Me.Controls.Add(Me.lblSectorDesign)
+        Me.Controls.Add(Me.lblNumberOfSeatsDesign)
+        Me.Controls.Add(Me.lblAirplaneModelDesign)
+        Me.Controls.Add(Me.btnAirplaneDetails)
+        Me.Controls.Add(Me.dataGridFuel)
+        Me.Controls.Add(Me.btnComputeFuel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmAirplaneData"
+        Me.Text = "Airplane Data"
+        CType(Me.dataGridFuel, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents btnComputeFuel As Button
+    Friend WithEvents dataGridFuel As DataGridView
+    Friend WithEvents btnAirplaneDetails As Button
+    Friend WithEvents lblAirplaneModelDesign As Label
+    Friend WithEvents lblNumberOfSeatsDesign As Label
+    Friend WithEvents lblSectorDesign As Label
+    Friend WithEvents lblFuelBurnDesign As Label
+    Friend WithEvents lblFuelPerSeatDesign As Label
+    Friend WithEvents lblRoundTripFuelDesign As Label
+    Friend WithEvents lblAirplaneModel As Label
+    Friend WithEvents lblNumberOfSeats As Label
+    Friend WithEvents lblSector As Label
+    Friend WithEvents lblFuelBurn As Label
+    Friend WithEvents lblFuelPerSeat As Label
+    Friend WithEvents lblRoundTripFuel As Label
 End Class
--- College/Lab 02/Lab 02/Form1.vb
+++ College/Lab 02/Lab 02/Form1.vb
@@ -1,5 +1,220 @@
-﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Structure Airplane
+    Dim model As String
+    Dim firstFlightYear As Integer
+    Dim seatCount As Integer
+    Dim sector As Integer
+    Dim fuelBurnRate As Double
+    Dim fuelPerSeat As Double
+End Structure
+
+Public Class frmAirplaneData
+
+    ' Publicly stored value for input
+    Public Shared input As String = ""
+
+    ' Fires a function that displays airplane fuel chart when button is pressed.
+    Private Sub btnComputeFuel_Click(sender As Object, e As EventArgs) Handles btnComputeFuel.Click
+        ' Updates chart displaying models with fuel per gallon/seat
+        DisplayComputeFuel()
+    End Sub
+
+    ' Fires a function that displays airplane details when button is pressed.
+    Private Sub btnAirplaneDetails_Click(sender As Object, e As EventArgs) Handles btnAirplaneDetails.Click
+        ' Updates airplane details displayed from entering a model.
+        DisplayAirplaneDetails()
+    End Sub
+
+    ' Calculates the amount of a specific plane
+    Function NumberOfPlane(ByVal planeName As String) As Integer
+
+        Dim planeArray As Airplane() = {}
+        Dim planeDataArray As String() = IO.File.ReadAllLines("Airplane.txt")
+
+        Array.Resize(planeArray, planeDataArray.Length)
+
+        For index = 0 To planeArray.Count - 1 Step 1
+            Dim localAirplane As New Airplane
+            Dim planeStr() As String = planeDataArray(index).Split(",")
+
+            localAirplane.model = planeStr(0)
+            localAirplane.firstFlightYear = CInt(planeStr(1))
+            localAirplane.seatCount = CInt(planeStr(2))
+            localAirplane.sector = CInt(planeStr(3))
+            localAirplane.fuelBurnRate = CDbl(planeStr(4))
+            localAirplane.fuelPerSeat = CDbl(planeStr(5))
+
+            planeArray(index) = localAirplane
+        Next
+
+        Dim query = From plane In planeArray
+                    Where plane.model.Contains(planeName)
+
+        Return query.Count
+    End Function
+
+    ' Calculates round trip fuel by doubling sector (fuel one way)
+    Function RoundTripFuel(ByVal sector As Integer) As Integer
+        Return (sector * 2)
+    End Function
+
+    ' Calculates a decimal value that calculates the average amount between two values.
+    Function CalculateAverages(ByVal dividend As Double, ByVal divisor As Integer) As Double
+        Return Math.Round(dividend / divisor, 1)
+    End Function
+
+    ' Updates input that is stored for airplane name.
+    Sub GetInput()
+        input = InputBox("Enter the Airplane Model", "Airplane Data")
+    End Sub
+
+    ' Updates chart displaying models with fuel per gallon/seat
+    Sub DisplayComputeFuel()
+        Dim planeArray As Airplane() = {}
+        Dim planeDataArray As String() = IO.File.ReadAllLines("Airplane.txt")
+
+        Array.Resize(planeArray, planeDataArray.Length)
+
+        For index = 0 To planeArray.Count - 1 Step 1
+            Dim localAirplane As New Airplane
+            Dim planeStr() As String = planeDataArray(index).Split(",")
+
+            localAirplane.model = planeStr(0)
+            localAirplane.firstFlightYear = CInt(planeStr(1))
+            localAirplane.seatCount = CInt(planeStr(2))
+            localAirplane.sector = CInt(planeStr(3))
+            localAirplane.fuelBurnRate = CDbl(planeStr(4))
+            localAirplane.fuelPerSeat = CDbl(planeStr(5))
+
+            planeArray(index) = localAirplane
+        Next
+
+        Dim planeDictionary As New Dictionary(Of String, Double)
+        Dim planeAmountDictionary As New Dictionary(Of String, Integer)
+
+        For index = 0 To planeArray.Count - 1 Step 1
+            Dim localPlane As Airplane = planeArray(index)
+            Dim name As String = localPlane.model.Split("-")(0)
+
+            If planeDictionary.ContainsKey(name) Then
+                Dim currentFuel As Double = planeDictionary.Item(name) + localPlane.fuelPerSeat
+                Dim planeCount As Integer = planeAmountDictionary.Item(name) + 1
+
+                planeDictionary.Item(name) = currentFuel
+                planeAmountDictionary.Item(name) = planeCount
+
+                Continue For
+            End If
+
+            planeDictionary.Add(name, localPlane.fuelPerSeat)
+            planeAmountDictionary.Add(name, 1)
+        Next
+
+        Dim query = From planeName As String In planeDictionary.Keys.ToList
+                    Let fuelPerSeat = CalculateAverages(planeDictionary.Item(planeName), planeAmountDictionary.Item(planeName))
+                    Order By fuelPerSeat Descending
+                    Select planeName, fuelPerSeat
+
+        dataGridFuel.DataSource = Nothing
+        dataGridFuel.Rows.Clear()
+
+        dataGridFuel.RowHeadersVisible = False
+
+        dataGridFuel.DataSource = query.ToList
+        dataGridFuel.CurrentCell = Nothing
+
+        dataGridFuel.Columns(0).HeaderText = "Model"
+        dataGridFuel.Columns("fuelPerSeat").HeaderText = "Fuel per gallon/seat"
+
+    End Sub
+
+    ' Updates airplane details displayed from entering a model.
+    Sub DisplayAirplaneDetails()
+        Dim planeArray As Airplane() = {}
+        Dim planeDataArray As String() = IO.File.ReadAllLines("Airplane.txt")
+
+        Array.Resize(planeArray, planeDataArray.Length)
+
+        For index = 0 To planeArray.Count - 1 Step 1
+            Dim localAirplane As New Airplane
+            Dim planeStr() As String = planeDataArray(index).Split(",")
+
+            localAirplane.model = planeStr(0)
+            localAirplane.firstFlightYear = CInt(planeStr(1))
+            localAirplane.seatCount = CInt(planeStr(2))
+            localAirplane.sector = CInt(planeStr(3))
+            localAirplane.fuelBurnRate = CDbl(planeStr(4))
+            localAirplane.fuelPerSeat = CDbl(planeStr(5))
+
+            planeArray(index) = localAirplane
+        Next
+
+        input = ""
+
+        While input = ""
+            GetInput()
+        End While
+
+        Dim valid As Boolean = False
+
+        For index = 0 To planeArray.Count - 1 Step 1
+            Dim localAirplane As Airplane = planeArray(index)
+            If localAirplane.model.Contains(input) Then
+                valid = True
+                Exit For
+            End If
+        Next
+
+        If Not valid Then
+            MessageBox.Show(“There is No known ” & input & “ Airplane Model”)
+            Return
+        End If
+
+        Dim planeCount As Integer = NumberOfPlane(input)
+        Dim avgSeats As Double = 0D
+        Dim sector As Integer = 0
+        Dim fuelBurn As Double = 0D
+        Dim fuelPerSeat As Double = 0D
+        Dim totalRoundTrip As Integer = 0
+
+        For index = 0 To planeArray.Count - 1 Step 1
+            Dim localAirplane As Airplane = planeArray(index)
+
+            If localAirplane.model.Contains(input) Then
+                avgSeats += localAirplane.seatCount
+                sector += localAirplane.sector
+                fuelBurn += localAirplane.fuelBurnRate
+                fuelPerSeat += localAirplane.fuelPerSeat
+                totalRoundTrip += RoundTripFuel(localAirplane.sector)
+            End If
+        Next
+
+        avgSeats = CalculateAverages(avgSeats, planeCount)
+        sector = CalculateAverages(sector, planeCount)
+        fuelBurn = CalculateAverages(fuelBurn, planeCount)
+        fuelPerSeat = CalculateAverages(fuelPerSeat, planeCount)
+
+        lblAirplaneModel.Text = input
+        lblNumberOfSeats.Text = CStr(avgSeats)
+        lblSector.Text = sector.ToString("N0")
+        lblFuelBurn.Text = Math.Round(fuelBurn, 1)
+        lblFuelPerSeat.Text = CStr(fuelPerSeat)
+        lblRoundTripFuel.Text = totalRoundTrip.ToString("N0")
 
     End Sub
 End Class
