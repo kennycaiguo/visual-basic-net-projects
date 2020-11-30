9273ad19656242e6f6dba8a6c460304cbd71bcfe
kyematzen <kyleofficalhd@gmail.com>
2020-11-30 14:12:17 -0500

    Added new project Chapter 8 P 428 Q19

---
+++ College/Chapter 8 P 428 Q19/Chapter 8 P 428 Q19/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCowboys
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCreateFile = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCreateFile
+        '
+        Me.btnCreateFile.Location = New System.Drawing.Point(100, 80)
+        Me.btnCreateFile.Name = "btnCreateFile"
+        Me.btnCreateFile.Size = New System.Drawing.Size(98, 41)
+        Me.btnCreateFile.TabIndex = 0
+        Me.btnCreateFile.Text = "Create File"
+        Me.btnCreateFile.UseVisualStyleBackColor = True
+        '
+        'frmCowboys
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 205)
+        Me.Controls.Add(Me.btnCreateFile)
+        Me.Name = "frmCowboys"
+        Me.Text = "Cowboys"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnCreateFile As Button
+End Class
---
+++ College/Chapter 8 P 428 Q19/Chapter 8 P 428 Q19/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class frmCowboys
+    Private Sub btnCreateFile_Click(sender As Object, e As EventArgs) Handles btnCreateFile.Click
+        Dim lines As String() = {"Colt Peacemaker,12.20", "Holster,2.00", "Levi Strauss jeans,1.35", "Saddle,40.00", "Stetson,10.00"}
+
+        IO.File.WriteAllLines("C:\Users\kyleo\Desktop\Cowboy.txt", lines)
+    End Sub
+End Class
