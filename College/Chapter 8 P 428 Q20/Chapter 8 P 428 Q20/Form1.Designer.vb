45edb4d555f72bc7ca27301dd63b0d9518044593
kyematzen <kyleofficalhd@gmail.com>
2020-11-30 14:30:50 -0500

    Updated project Chapter 8 P 428 Q20

--- College/Chapter 8 P 428 Q20/Chapter 8 P 428 Q20/Form1.Designer.vb
+++ College/Chapter 8 P 428 Q20/Chapter 8 P 428 Q20/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmCowboys
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnCreateFile = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCreateFile
+        '
+        Me.btnCreateFile.Location = New System.Drawing.Point(100, 80)
+        Me.btnCreateFile.Name = "btnCreateFile"
+        Me.btnCreateFile.Size = New System.Drawing.Size(98, 41)
+        Me.btnCreateFile.TabIndex = 0
+        Me.btnCreateFile.Text = "Create File"
+        Me.btnCreateFile.UseVisualStyleBackColor = True
+        '
+        'frmCowboys
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(307, 205)
+        Me.Controls.Add(Me.btnCreateFile)
+        Me.Name = "frmCowboys"
+        Me.Text = "Cowboys"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents btnCreateFile As Button
 End Class
--- College/Chapter 8 P 428 Q20/Chapter 8 P 428 Q20/Form1.vb
+++ College/Chapter 8 P 428 Q20/Chapter 8 P 428 Q20/Form1.vb
@@ -1,3 +1,19 @@
-﻿Public Class Form1
+﻿Public Class frmCowboys
+    Private Sub btnCreateFile_Click(sender As Object, e As EventArgs) Handles btnCreateFile.Click
+        Dim cowboyFile As String() = IO.File.ReadAllLines("C:\Users\kyleo\Desktop\Cowboy.txt")
 
+        For index = 0 To cowboyFile.Count - 1 Step 1
+            Dim localLine = cowboyFile(index)
+
+            If (localLine.StartsWith("Saddle")) Then
+                Dim lineSplit = localLine.Split(",")
+                Dim amount As Double = CDbl(lineSplit(1))
+
+                cowboyFile(index) = "Saddle," & (amount * 0.8)
+                Exit For
+            End If
+        Next
+
+        IO.File.WriteAllLines("C:\Users\kyleo\Desktop\Cowboy.txt", cowboyFile)
+    End Sub
 End Class
