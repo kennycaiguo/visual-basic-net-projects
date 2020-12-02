b16307e1fd342d248f864dd3bf2916de4e6be0d3
kyematzen <kyleofficalhd@gmail.com>
2020-12-02 17:43:34 -0500

    Added new project Chapter 8 P 428 Q21

---
+++ College/Chapter 8 P 428 Q22/Chapter 8 P 428 Q22/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCowboys
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.listBoxReceipt = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(37, 12)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(122, 23)
+        Me.btnDisplay.TabIndex = 0
+        Me.btnDisplay.Text = "Display Sales Receipt"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'listBoxReceipt
+        '
+        Me.listBoxReceipt.FormattingEnabled = True
+        Me.listBoxReceipt.Location = New System.Drawing.Point(12, 41)
+        Me.listBoxReceipt.Name = "listBoxReceipt"
+        Me.listBoxReceipt.Size = New System.Drawing.Size(175, 82)
+        Me.listBoxReceipt.TabIndex = 1
+        '
+        'frmCowboys
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(199, 129)
+        Me.Controls.Add(Me.listBoxReceipt)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmCowboys"
+        Me.Text = "Cowboys"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents listBoxReceipt As ListBox
+End Class
---
+++ College/Chapter 8 P 428 Q22/Chapter 8 P 428 Q22/Form1.vb
@@ -0,0 +1,58 @@
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class frmCowboys
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        ' Creates order array
+        Dim order As String() = {"3", "2", "10", "1", "4"}
+
+        ' Writes order file to project storage.
+        IO.File.WriteAllLines("Order.txt", order)
+
+        ' Checks if cowboy previously exists.
+        If Not IO.File.Exists("Cowboy.txt") Then
+            ' Creates cowboy product array with pricing
+            Dim lines As String() = {"Colt Peacemaker,12.20", "Holster,2.00", "Levi Strauss jeans,1.35", "Saddle,40.00", "Stetson,10.00"}
+
+            ' Writes cowboy file to project storage.
+            IO.File.WriteAllLines("Cowboy.txt", lines)
+        End If
+
+        ' Reads cowboy file from project storage.
+        Dim cowboyFile As String() = IO.File.ReadAllLines("Cowboy.txt")
+
+        ' Creates localized variable for total receipt of sales.
+        Dim totalReceipt As Double = 0
+
+        ' Loops from first index of order array to end to calculate costs of cowboy products.
+        For index As Integer = 0 To order.Count - 1 Step 1
+            ' Splits line grabbed from cowboy file by a comma to find name of product and price.
+            Dim itemInfo As String() = cowboyFile(index).Split(",")
+
+            ' Finds price for number of product.
+            Dim value As Double = CInt(order(index)) * CDbl(itemInfo(1))
+
+            ' Adds to receipt information the amount of product, name, and price for number of product.
+            listBoxReceipt.Items.Add(order(index) & " " & itemInfo(0) & ": " & value.ToString("C"))
+
+            ' Adds value of product to total receipt for amount ordered.
+            totalReceipt += value
+        Next
+
+        ' Adds to receipt information the total cost for all products from sale receipt.
+        listBoxReceipt.Items.Add("TOTAL: " & totalReceipt.ToString("C"))
+    End Sub
+End Class
