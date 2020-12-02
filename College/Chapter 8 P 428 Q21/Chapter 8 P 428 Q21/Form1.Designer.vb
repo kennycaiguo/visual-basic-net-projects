273faa6d55dba5afef504a94c6368bd02c450304
kyematzen <kyleofficalhd@gmail.com>
2020-12-02 17:44:28 -0500

    Added new project Chapter 8 P 428 Q21

---
+++ College/Chapter 8 P 428 Q21/Chapter 8 P 428 Q21/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCowboys
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnUpdateFile = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnUpdateFile
+        '
+        Me.btnUpdateFile.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnUpdateFile.Location = New System.Drawing.Point(100, 80)
+        Me.btnUpdateFile.Name = "btnUpdateFile"
+        Me.btnUpdateFile.Size = New System.Drawing.Size(98, 41)
+        Me.btnUpdateFile.TabIndex = 0
+        Me.btnUpdateFile.Text = "Update File"
+        Me.btnUpdateFile.UseVisualStyleBackColor = True
+        '
+        'frmCowboys
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 205)
+        Me.Controls.Add(Me.btnUpdateFile)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Name = "frmCowboys"
+        Me.Text = "Cowboys"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnUpdateFile As Button
+End Class
---
+++ College/Chapter 8 P 428 Q21/Chapter 8 P 428 Q21/Form1.vb
@@ -0,0 +1,40 @@
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class frmCowboys
+    Private Sub btnCreateFile_Click(sender As Object, e As EventArgs) Handles btnUpdateFile.Click
+        ' Checks if cowboy previously exists.
+        If Not IO.File.Exists("Cowboy.txt") Then
+            ' Creates cowboy product array with pricing
+            Dim lines As String() = {"Colt Peacemaker,12.20", "Holster,2.00", "Levi Strauss jeans,1.35", "Saddle,40.00", "Stetson,10.00"}
+
+            ' Writes cowboy file to project storage.
+            IO.File.WriteAllLines("Cowboy.txt", lines)
+        End If
+
+        ' Reads cowboy file from project storage.
+        Dim cowboyFile As String() = IO.File.ReadAllLines("C:\Users\kyleo\Desktop\Cowboy.txt")
+
+        ' Resizes array of cowboy file plus one to add new item.
+        Array.Resize(cowboyFile, cowboyFile.Length + 1)
+
+        ' Adds Winchester Rifle to add of cowboy file.
+        cowboyFile(cowboyFile.Length - 1) = "Winchester Rifle,20.50"
+
+        ' Writes cowboy file to project storage.
+        IO.File.WriteAllLines("C:\Users\kyleo\Desktop\Cowboy.txt", cowboyFile)
+    End Sub
+End Class
