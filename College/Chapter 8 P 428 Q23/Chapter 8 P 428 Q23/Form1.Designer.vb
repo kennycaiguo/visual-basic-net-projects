552df897839c992986fd60748d216f60d2793f56
kyematzen <kyleofficalhd@gmail.com>
2020-12-06 03:15:21 -0500

    Added new project Chapter 8 P 428 Q23

---
+++ College/Chapter 8 P 428 Q23/Chapter 8 P 428 Q23/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCowboys
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblItem = New System.Windows.Forms.Label()
+        Me.txtBoxItem = New System.Windows.Forms.TextBox()
+        Me.lblPrice = New System.Windows.Forms.Label()
+        Me.txtBoxPrice = New System.Windows.Forms.TextBox()
+        Me.btnAddItem = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblItem
+        '
+        Me.lblItem.AutoSize = True
+        Me.lblItem.Location = New System.Drawing.Point(12, 9)
+        Me.lblItem.Name = "lblItem"
+        Me.lblItem.Size = New System.Drawing.Size(30, 13)
+        Me.lblItem.TabIndex = 0
+        Me.lblItem.Text = "Item:"
+        '
+        'txtBoxItem
+        '
+        Me.txtBoxItem.Location = New System.Drawing.Point(48, 6)
+        Me.txtBoxItem.Name = "txtBoxItem"
+        Me.txtBoxItem.Size = New System.Drawing.Size(58, 20)
+        Me.txtBoxItem.TabIndex = 1
+        '
+        'lblPrice
+        '
+        Me.lblPrice.AutoSize = True
+        Me.lblPrice.Location = New System.Drawing.Point(112, 9)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(34, 13)
+        Me.lblPrice.TabIndex = 2
+        Me.lblPrice.Text = "Price:"
+        '
+        'txtBoxPrice
+        '
+        Me.txtBoxPrice.Location = New System.Drawing.Point(152, 6)
+        Me.txtBoxPrice.Name = "txtBoxPrice"
+        Me.txtBoxPrice.Size = New System.Drawing.Size(58, 20)
+        Me.txtBoxPrice.TabIndex = 3
+        '
+        'btnAddItem
+        '
+        Me.btnAddItem.Location = New System.Drawing.Point(81, 32)
+        Me.btnAddItem.Name = "btnAddItem"
+        Me.btnAddItem.Size = New System.Drawing.Size(92, 23)
+        Me.btnAddItem.TabIndex = 4
+        Me.btnAddItem.Text = "Add Item to List"
+        Me.btnAddItem.UseVisualStyleBackColor = True
+        '
+        'frmCowboys
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(224, 62)
+        Me.Controls.Add(Me.btnAddItem)
+        Me.Controls.Add(Me.txtBoxPrice)
+        Me.Controls.Add(Me.lblPrice)
+        Me.Controls.Add(Me.txtBoxItem)
+        Me.Controls.Add(Me.lblItem)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmCowboys"
+        Me.Text = "Cowboys"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblItem As Label
+    Friend WithEvents txtBoxItem As TextBox
+    Friend WithEvents lblPrice As Label
+    Friend WithEvents txtBoxPrice As TextBox
+    Friend WithEvents btnAddItem As Button
+End Class
---
+++ College/Chapter 8 P 428 Q23/Chapter 8 P 428 Q23/Form1.vb
@@ -0,0 +1,83 @@
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class frmCowboys
+    Private Sub btnAddItem_Click(sender As Object, e As EventArgs) Handles btnAddItem.Click
+        ' Checks if a item has been entered/is valid.
+
+
+        If txtBoxItem.Text = "" OrElse txtBoxItem.Text.Trim = "" Then
+            MessageBox.Show("Please enter a valid item name.", "Cowboys")
+            Return
+        End If
+
+        If txtBoxPrice.Text = "" OrElse txtBoxPrice.Text.Trim = "" OrElse Not IsNumeric(txtBoxPrice.Text) Then
+            MessageBox.Show("Please enter a valid price.", "Cowboys")
+            Return
+        End If
+
+        ' Checks if a price has been entered/is valid.
+
+        ' Checks if cowboy previously exists.
+        If Not IO.File.Exists("Cowboy.txt") Then
+            ' Creates cowboy product array with pricing
+            Dim lines As String() = {"Colt Peacemaker,12.20", "Holster,2.00", "Levi Strauss jeans,1.35", "Saddle,40.00", "Stetson,10.00"}
+
+            ' Writes cowboy file to project storage.
+            IO.File.WriteAllLines("Cowboy.txt", lines)
+        End If
+
+        ' Reads cowboy file from project storage.
+        Dim cowboyFile As String() = IO.File.ReadAllLines("Cowboy.txt")
+
+        ' Create dictionary of name (string) and price (double)
+        Dim dictionary As Dictionary(Of String, Double) = New Dictionary(Of String, Double)
+
+        ' Add items from cowboy file into dictionary.
+
+        For index As Integer = 0 To cowboyFile.Count - 1 Step 1
+            Dim split As String() = cowboyFile(index).Split(",")
+
+            dictionary.Add(split(0), CDbl(split(1)))
+        Next
+
+        ' Adds new item to dictionary of original items.
+        dictionary.Add(txtBoxItem.Text, CDbl(txtBoxPrice.Text))
+
+        ' Loops through all item names in dictionary, and sorts by pricing
+        Dim query = From name As String In dictionary.Keys.ToList
+                    Order By name Ascending
+                    Select name
+
+        ' Converts query into a array of names
+        Dim itemArray As String() = query.ToList.ToArray
+
+        ' Creates new local array for Cowboy2.txt
+        Dim cowboy2Array As String() = {}
+
+        ' Resizes array
+
+        Array.Resize(cowboy2Array, itemArray.Count)
+
+        ' Updates array with new information from query sorted. 
+        For index As Integer = 0 To itemArray.Count - 1 Step 1
+            cowboy2Array(index) = itemArray(index) & "," & dictionary(itemArray(index))
+        Next
+
+        ' Writes cowboy2 file to project storage.
+        IO.File.WriteAllLines("Cowboy2.txt", cowboy2Array)
+    End Sub
+End Class
