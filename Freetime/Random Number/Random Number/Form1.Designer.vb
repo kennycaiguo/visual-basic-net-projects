c95fc1d09e57d7782107391eeec43de672408384
kyematzen <kyleofficalhd@gmail.com>
2020-12-06 05:01:49 -0500

    Added new project Random Number

---
+++ Freetime/Random Number/Random Number/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblRandomNumber = New System.Windows.Forms.Label()
+        Me.lblRandomDouble = New System.Windows.Forms.Label()
+        Me.lblRandomRounded = New System.Windows.Forms.Label()
+        Me.lblRandomInteger = New System.Windows.Forms.Label()
+        Me.lblRandomRange = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblRandomNumber
+        '
+        Me.lblRandomNumber.AutoSize = True
+        Me.lblRandomNumber.Location = New System.Drawing.Point(13, 13)
+        Me.lblRandomNumber.Name = "lblRandomNumber"
+        Me.lblRandomNumber.Size = New System.Drawing.Size(90, 13)
+        Me.lblRandomNumber.TabIndex = 0
+        Me.lblRandomNumber.Text = "Random Number:"
+        '
+        'lblRandomDouble
+        '
+        Me.lblRandomDouble.AutoSize = True
+        Me.lblRandomDouble.Location = New System.Drawing.Point(13, 26)
+        Me.lblRandomDouble.Name = "lblRandomDouble"
+        Me.lblRandomDouble.Size = New System.Drawing.Size(87, 13)
+        Me.lblRandomDouble.TabIndex = 1
+        Me.lblRandomDouble.Text = "Random Double:"
+        '
+        'lblRandomRounded
+        '
+        Me.lblRandomRounded.AutoSize = True
+        Me.lblRandomRounded.Location = New System.Drawing.Point(12, 39)
+        Me.lblRandomRounded.Name = "lblRandomRounded"
+        Me.lblRandomRounded.Size = New System.Drawing.Size(97, 13)
+        Me.lblRandomRounded.TabIndex = 2
+        Me.lblRandomRounded.Text = "Random Rounded:"
+        '
+        'lblRandomInteger
+        '
+        Me.lblRandomInteger.AutoSize = True
+        Me.lblRandomInteger.Location = New System.Drawing.Point(12, 52)
+        Me.lblRandomInteger.Name = "lblRandomInteger"
+        Me.lblRandomInteger.Size = New System.Drawing.Size(86, 13)
+        Me.lblRandomInteger.TabIndex = 3
+        Me.lblRandomInteger.Text = "Random Integer:"
+        '
+        'lblRandomRange
+        '
+        Me.lblRandomRange.AutoSize = True
+        Me.lblRandomRange.Location = New System.Drawing.Point(12, 65)
+        Me.lblRandomRange.Name = "lblRandomRange"
+        Me.lblRandomRange.Size = New System.Drawing.Size(85, 13)
+        Me.lblRandomRange.TabIndex = 4
+        Me.lblRandomRange.Text = "Random Range:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 128)
+        Me.Controls.Add(Me.lblRandomRange)
+        Me.Controls.Add(Me.lblRandomInteger)
+        Me.Controls.Add(Me.lblRandomRounded)
+        Me.Controls.Add(Me.lblRandomDouble)
+        Me.Controls.Add(Me.lblRandomNumber)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblRandomNumber As Label
+    Friend WithEvents lblRandomDouble As Label
+    Friend WithEvents lblRandomRounded As Label
+    Friend WithEvents lblRandomInteger As Label
+    Friend WithEvents lblRandomRange As Label
+End Class
---
+++ Freetime/Random Number/Random Number/Form1.vb
@@ -0,0 +1,47 @@
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Declares instance of Random as randomNum
+        Dim randomNum As New Random()
+
+        ' Scenarios
+        ' 1. If m and n are whole numbers with m < n then the following generates a random whole number between m and n
+        ' Exclusive of n.
+        ' randomNum.Next(m, n)
+
+        lblRandomNumber.Text = "Random Number: " & randomNum.Next(2, 18)
+
+        ' 2. Returns a floating double number that is greater than or equal to 0.0, and less than 1.0.
+        ' randomNum.NextDouble()
+        lblRandomDouble.Text = "Random Double: " & randomNum.NextDouble()
+
+        ' 2. Returns a floating double number that is greater than or equal to 0.0, and less than 1.0 rounded to 2 decimals.
+        ' Math.Round(randomNum.NextDouble(), 2)
+
+        lblRandomRounded.Text = "Random Rounded: " & Math.Round(randomNum.NextDouble(), 2)
+
+        ' 3. Returns a non-negative integer number.
+        ' randomNum.Next()
+        lblRandomInteger.Text = "Random Integer: " & randomNum.Next()
+
+        ' 3. Returns a non-negative integer number within range of n.
+        ' Exclusive of n.
+        ' randomNum.Next(n)
+        lblRandomRange.Text = "Random Range: " & randomNum.Next(5)
+    End Sub
+End Class
