656112ba66df891663eaee95e595c015358c3c0b
kyematzen <kyleofficalhd@gmail.com>
2020-12-06 03:14:17 -0500

    Added new project Chapter 8 P 448 U.S Senate

---
+++ College/Chapter 8 P 448 U.S Senate/Chapter 8 P 448 U.S Senate/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUSSenate
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCreateFile = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCreateFile
+        '
+        Me.btnCreateFile.Location = New System.Drawing.Point(35, 24)
+        Me.btnCreateFile.Name = "btnCreateFile"
+        Me.btnCreateFile.Size = New System.Drawing.Size(75, 23)
+        Me.btnCreateFile.TabIndex = 0
+        Me.btnCreateFile.Text = "Create File"
+        Me.btnCreateFile.UseVisualStyleBackColor = True
+        '
+        'frmUSSenate
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(144, 75)
+        Me.Controls.Add(Me.btnCreateFile)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmUSSenate"
+        Me.Text = "U.S Senate"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnCreateFile As Button
+End Class
---
+++ College/Chapter 8 P 448 U.S Senate/Chapter 8 P 448 U.S Senate/Form1.vb
@@ -0,0 +1,38 @@
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class frmUSSenate
+    Private Sub btnCreateFile_Click(sender As Object, e As EventArgs) Handles btnCreateFile.Click
+        ' Grabs information from files
+        Dim firstFile As String() = IO.File.ReadAllLines("Senate115.txt")
+        Dim secondFile As String() = IO.File.ReadAllLines("RetiredSen.txt")
+        Dim thirdFile As String() = IO.File.ReadAllLines("NewSen.txt")
+
+        ' Concatations arrays together.
+        Dim finalArray As String() = firstFile.Concat(secondFile.Concat(thirdFile).ToArray).ToArray
+
+        ' Loops through final array and splits by a comma, and grabs the state, and orders by state, and finally selects the line.
+        Dim query = From line In finalArray
+                    Let lineSplit As String() = line.Split(",")
+                    Let state As String = lineSplit(1)
+                    Order By state
+                    Select line
+
+        ' Write Senate116.txt to project storage
+        IO.File.WriteAllLines("Senate116.txt", query.ToList.ToArray)
+
+    End Sub
+End Class
