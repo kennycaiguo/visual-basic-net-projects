63075cd2e30a4962d58f54ea765df8b00864fdca
kyematzen <kyleofficalhd@gmail.com>
2020-12-13 19:16:27 -0500

    Added new project Contestant Output

---
+++ Freetime/Contestant Output/Contestant Output/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmContestants
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(12, 12)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(120, 95)
+        Me.lstOutput.TabIndex = 0
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(167, 47)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(75, 23)
+        Me.btnDisplay.TabIndex = 1
+        Me.btnDisplay.Text = "Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'frmContestants
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(281, 119)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.lstOutput)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmContestants"
+        Me.Text = "Contestants"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstOutput As ListBox
+    Friend WithEvents btnDisplay As Button
+End Class
---
+++ Freetime/Contestant Output/Contestant Output/Form1.vb
@@ -0,0 +1,37 @@
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class frmContestants
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        Dim randomNum As New Random
+        Dim contestant As String() = {"Mary", "Pat", "Linda", "Barbara", "Maria"}
+
+        Dim number As Integer, temp As String
+
+        For i As Integer = 0 To 4
+            number = randomNum.Next(i, 5)
+            temp = contestant(i)
+            contestant(i) = contestant(number)
+            contestant(number) = temp
+        Next
+
+        lstOutput.Items.Clear()
+
+        For i As Integer = 0 To 4
+            lstOutput.Items.Add(contestant(i))
+        Next
+    End Sub
+End Class
