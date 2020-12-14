567059de7038e92122b7a9d217855f1183e95921
kyematzen <kyleofficalhd@gmail.com>
2020-12-13 19:18:33 -0500

    Added new project Rock Paper-Scissors

---
+++ Freetime/Rock Paper-Scissors/Rock Paper-Scissors/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRPS
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSelection = New System.Windows.Forms.Button()
+        Me.txtBoxSelected = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnSelection
+        '
+        Me.btnSelection.Location = New System.Drawing.Point(29, 12)
+        Me.btnSelection.Name = "btnSelection"
+        Me.btnSelection.Size = New System.Drawing.Size(153, 23)
+        Me.btnSelection.TabIndex = 0
+        Me.btnSelection.Text = "Randomly Make A Selection"
+        Me.btnSelection.UseVisualStyleBackColor = True
+        '
+        'txtBoxSelected
+        '
+        Me.txtBoxSelected.Location = New System.Drawing.Point(56, 41)
+        Me.txtBoxSelected.Name = "txtBoxSelected"
+        Me.txtBoxSelected.ReadOnly = True
+        Me.txtBoxSelected.Size = New System.Drawing.Size(100, 20)
+        Me.txtBoxSelected.TabIndex = 1
+        '
+        'frmRPS
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(219, 81)
+        Me.Controls.Add(Me.txtBoxSelected)
+        Me.Controls.Add(Me.btnSelection)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmRPS"
+        Me.Text = "Rock-Paper-Scissors"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSelection As Button
+    Friend WithEvents txtBoxSelected As TextBox
+End Class
---
+++ Freetime/Rock Paper-Scissors/Rock Paper-Scissors/Form1.vb
@@ -0,0 +1,30 @@
+﻿
+' 
+' Copyright 2020, Kye Matzen, http://kyematzen.com
+' <p>
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+' Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+' and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+' <p>
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+' <p>
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Public Class frmRPS
+    Private Sub btnSelection_Click(sender As Object, e As EventArgs) Handles btnSelection.Click
+        Dim choices As String() = {"Rock", "Paper", "Scissors"}
+
+        ' Declares instance of Random in randomNum
+        Dim randomNum As New Random
+
+        ' Randomly grabs a value from 0-2 to grab one of the choices.
+        Dim n As Integer = randomNum.Next(minValue:=0, maxValue:=3) ' Randomly selects  0,1, or 2
+
+        ' Updates what is selected, after button is clicked.
+        txtBoxSelected.Text = choices(n)
+    End Sub
+End Class
