6d5e8453ba964a4950d1fd609b41eb1c23ba4059
kyematzen <kyleofficalhd@gmail.com>
2020-12-13 19:15:48 -0500

    Added new project Population Density

---
+++ Book/Population Density/Population Density/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDensity
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.listBoxOutput = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(77, 12)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(91, 23)
+        Me.btnDisplay.TabIndex = 0
+        Me.btnDisplay.Text = "Display Density"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'listBoxOutput
+        '
+        Me.listBoxOutput.FormattingEnabled = True
+        Me.listBoxOutput.Location = New System.Drawing.Point(21, 41)
+        Me.listBoxOutput.Name = "listBoxOutput"
+        Me.listBoxOutput.Size = New System.Drawing.Size(203, 95)
+        Me.listBoxOutput.TabIndex = 1
+        '
+        'frmDensity
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(252, 147)
+        Me.Controls.Add(Me.listBoxOutput)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmDensity"
+        Me.Text = "Density of a State"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents listBoxOutput As ListBox
+End Class
---
+++ Book/Population Density/Population Density/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class frmDensity
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        DescribeTask()
+        CalculateDensity("Hawaii", 1420000, 6471)
+    End Sub
+
+    Sub DescribeTask()
+        listBoxOutput.Items.Clear()
+        listBoxOutput.Items.Add("This population displays the")
+        listBoxOutput.Items.Add("population density of the last state")
+        listBoxOutput.Items.Add("to become part of the United States.")
+    End Sub
+
+    Sub CalculateDensity(state As String, pop As Double, landArea As Double)
+        Dim density As Double
+        density = pop / landArea
+        listBoxOutput.Items.Add("") 'insert a blank line
+        listBoxOutput.Items.Add("The population density of " & state & " is")
+        listBoxOutput.Items.Add(density.ToString("N1") & " people per square mile.")
+    End Sub
+End Class
